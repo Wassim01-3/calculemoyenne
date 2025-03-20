011899ffc6dc8336ad37bf782c933bea74dad045
Wassim01-3 <wassimmars0@gmail.com>
2025-03-21 00:41:27 +0100

    Update calcule1lsg1.php

--- calcule1lsg1.php
+++ calcule1lsg1.php
@@ -1,5 +1,6 @@
 <html lang="en">
 <head>
+    <meta name="google-adsense-account" content="ca-pub-5006802001974928">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="calcule.css">
